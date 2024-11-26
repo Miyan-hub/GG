4caa182d23d63bb24b12888462922c7ac509a8c4
Miyan0001 <mochamedandryan@gmail.com>
2024-11-27 04:45:49 +0700

    ne

--- gold and glory.lua
+++ gold and glory.lua
@@ -3,7 +3,12 @@ Il2cpp({il2cppVersion = 29})
 
 method = Il2cpp.FindMethods({
 "CanEquip",
-"IsSkillEnableWithWeapon"
+"IsSkillEnableWithWeapon",
+"get_CanJump",
+"CheckCanStandUp",
+"get_IsOpen",
+"get_NeedC1ondition",
+
 })
 t = {}
 for x = 1,#method do
@@ -17,13 +22,29 @@ gg.setValues(t)
 gg.addListItems(t)
 end
 method = Il2cpp.FindMethods({
-"IsMember"
+"get_NeedC1ondition",
+
 })
 t = {}
 for x = 1,#method do
 for i,v in ipairs(method[x]) do
 t[i] = {}
 t[i].address = "0x"..v.AddressInMemory
+t[i].value = "000080D2C0035FD6r"
+t[i].flags = 32
+end
+gg.setValues(t)
+gg.addListItems(t)
+end
+method = Il2cpp.FindMethods({
+"get_AirJumpCount"
+})
+t = {}
+for x = 1,#method do
+for i,v in ipairs(method[x]) do
+t[i] = {}
+t[i].address = "0x"..v.AddressInMemory
+t[i].value = "E0CF8952C0035FD6r"
 t[i].flags = 32
 end
 gg.addListItems(t)
