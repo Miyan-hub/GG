389b81beccac68297b2d5040f3469267187ef2d0
Miyan0001 <mochamedandryan@gmail.com>
2024-11-27 04:53:41 +0700

    ne

--- gold and glory.lua
+++ gold and glory.lua
@@ -1,51 +1,42 @@
 require('Il2cppApi')
 Il2cpp({il2cppVersion = 29})
 
-method = Il2cpp.FindMethods({
-"CanEquip",
-"IsSkillEnableWithWeapon",
-"get_CanJump",
-"CheckCanStandUp",
-"get_IsOpen",
-"get_NeedC1ondition",
+-- Define and process methods
+local methodGroups = {
+    {
+        methods = {"CanEquip", "IsSkillEnableWithWeapon", "get_CanJump", "CheckCanStandUp", "get_IsOpen"},
+        value = "200080D2C0035FD6r"
+    },
+    {
+        methods = {"get_NeedCondition"},
+        value = "000080D2C0035FD6r"
+    },
+    {
+        methods = {"get_AirJumpCount"},
+        value = "E0CF8952C0035FD6r"
+    }
+}
 
-})
-t = {}
-for x = 1,#method do
-for i,v in ipairs(method[x]) do
-t[i] = {}
-t[i].address = "0x"..v.AddressInMemory
-t[i].value = "200080D2C0035FD6r"
-t[i].flags = 32
+local t = {}
+local index = 1
+
+for _, group in ipairs(methodGroups) do
+    for _, methodName in ipairs(group.methods) do
+        local method = Il2cpp.FindMethods({methodName})
+
+        for x = 1, #method do
+            for _, v in ipairs(method[x]) do
+                t[index] = {
+                    address = "0x" .. v.AddressInMemory,
+                    value = group.value,
+                    flags = 32,
+                    name = methodName .. "\n" .. v.Offset
+                }
+                index = index + 1
+            end
+        end
+    end
 end
+
 gg.setValues(t)
 gg.addListItems(t)
-end
-method = Il2cpp.FindMethods({
-"get_NeedC1ondition",
-
-})
-t = {}
-for x = 1,#method do
-for i,v in ipairs(method[x]) do
-t[i] = {}
-t[i].address = "0x"..v.AddressInMemory
-t[i].value = "000080D2C0035FD6r"
-t[i].flags = 32
-end
-gg.setValues(t)
-gg.addListItems(t)
-end
-method = Il2cpp.FindMethods({
-"get_AirJumpCount"
-})
-t = {}
-for x = 1,#method do
-for i,v in ipairs(method[x]) do
-t[i] = {}
-t[i].address = "0x"..v.AddressInMemory
-t[i].value = "E0CF8952C0035FD6r"
-t[i].flags = 32
-end
-gg.addListItems(t)
-end
