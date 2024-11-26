c3fe523cbeaa436dc0c2283065adfed3eef6cbcd
Miyan0001 <mochamedandryan@gmail.com>
2024-11-26 16:05:50 +0700

    Update

--- gold and glory.lua
+++ gold and glory.lua
@@ -2,7 +2,8 @@ require('Il2cppApi')
 Il2cpp({il2cppVersion = 29})
 
 method = Il2cpp.FindMethods({
-"CanEquip"
+"CanEquip",
+"IsSkillEnableWithWeapon"
 })
 t = {}
 for x = 1,#method do
@@ -13,5 +14,17 @@ t[i].value = "200080D2C0035FD6r"
 t[i].flags = 32
 end
 gg.setValues(t)
+gg.addListItems(t)
+end
+method = Il2cpp.FindMethods({
+"IsMember"
+})
+t = {}
+for x = 1,#method do
+for i,v in ipairs(method[x]) do
+t[i] = {}
+t[i].address = "0x"..v.AddressInMemory
+t[i].flags = 32
+end
 gg.addListItems(t)
 end
